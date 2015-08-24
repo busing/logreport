8f8694eaf69432d35490d9edc30df3d9b65e85a3
taiyuan <taiyuan321@163.com>
2015-08-24 16:59:22 +0800

    修改异常日志

--- src/main/java/com/junrui/logreport/dbutil/mysql/MySqlHandler.java
+++ src/main/java/com/junrui/logreport/dbutil/mysql/MySqlHandler.java
@@ -2,10 +2,7 @@ package com.junrui.logreport.dbutil.mysql;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
-
 import org.apache.log4j.Logger;
-
 import com.junrui.logreport.constance.Configuration;
 
 public class MySqlHandler {
@@ -21,10 +18,8 @@ public class MySqlHandler {
 			Class.forName(DRIVER_CLASS);
 			connection=DriverManager.getConnection(Configuration.MYSQLURL,Configuration.MYSQLUSERNAME,Configuration.MYSQLPASSWORD);
 		
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		} catch (SQLException e) {
-			e.printStackTrace();
+		} catch (Exception e) {
+			log.error("获取mysql数据库连接异常",e);
 		}
 		return connection;
 	}
